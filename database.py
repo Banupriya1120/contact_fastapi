39f1b529cfb3a54dde6b1cab3c7b18b1dbc5b49e
Banu <banupriya.p1120@gmail.com>
2023-11-03 23:04:56 +0530

    db env vars

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
 
 
 
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:priya123@localhost/ContactsApplicationDatabase"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(DB_URL)
 SessionLocal =sessionmaker(autocommit=False, autoflush=False, bind=engine) 
 base = declarative_base()
